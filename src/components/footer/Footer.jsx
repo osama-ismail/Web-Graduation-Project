d39bdbd2052080c7a0d8863038f7ee730ad6eb16
Yazan-Habash114 <54286064+Yazan-Habash114@users.noreply.github.com>
2022-01-16 16:02:00 +0200

    Edit footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,12 @@
 import FooterSvg from "./FooterSvg";
 import styled from "styled-components";
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import MailOutlineRoundedIcon from '@mui/icons-material/MailOutlineRounded';
 
 const Ftr = styled.footer`
-    background-color: #282828;
+    background-color: #2f2f2f;
     padding: 10px;
 `
 
@@ -10,29 +14,57 @@ const Wrapper = styled.div`
     display: flex;
     justify-content: space-around;
     flex-flow: row wrap;
-    padding: 1em;
+    padding: 0.5em;
 `
 
 const Logo = styled.div`
     color: white;
+    text-align: center;
 `
 
 const Info = styled.div`
     color: white;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `
 
 const Email = styled.a`
     text-decoration: none;
     color: white;
+    text-align: center;
+    margin: 10px 5px;
+    display: flex;
+    align-items: center;
 `
 
-const SocialMediaLinks = styled.a`
-    text-decoration: none;
-    color: white;
+const SocialMedia = styled.section`
+    display: flex;
+    justify-content: space-around;
+    cursor: pointer;
+    margin: 10px 5px;
+    width: 100%;
 `
 
 const MobileApp = styled.div`
     color: white;
+    text-align: center;
+`
+
+const Separator = styled.div`
+    border: 1px solid #191919;
+`
+
+const CopyrightWrapper = styled.div`
+    padding: 1em;
+    text-align: center;
+    background-color: #212121;
+`
+
+const Copyright = styled.div`
+    color: white;
+    font-size: 12px;
+    letter-spacing: 2px;
 `
 
 const Footer = () => {
@@ -43,19 +75,24 @@ const Footer = () => {
                 <Wrapper>
                     <Logo>Site Logo Again (Image + Title)</Logo>
                     <Info>
-                        <Email href="mailto:project@website.com">Email</Email>
-                        <br />
+                        <Email href="mailto:project@website.com">
+                            <MailOutlineRoundedIcon />
+                            &nbsp;project@website.com
+                        </Email>
                         Phone
-                        <br />
-                        <SocialMediaLinks href="#">Facebook logo</SocialMediaLinks>
-                        <br />
-                        <SocialMediaLinks href="#">Instagram logo</SocialMediaLinks>
-                        <br />
-                        <SocialMediaLinks href="#">Linked In logo</SocialMediaLinks>
+                        <SocialMedia>
+                            <FacebookRoundedIcon />
+                            <InstagramIcon />
+                            <LinkedInIcon />
+                        </SocialMedia>
                     </Info>
                     <MobileApp>Mobile App Image</MobileApp>
                 </Wrapper>
             </Ftr>
+            <Separator />
+            <CopyrightWrapper>
+                <Copyright>Copyright © 2022 | All rights reserved</Copyright>
+            </CopyrightWrapper>
         </div>
     )
 }
