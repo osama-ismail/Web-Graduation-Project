9040485aa6dd68e8322edab9c5187797800119e3
Yazan-Habash114 <54286064+Yazan-Habash114@users.noreply.github.com>
2022-03-02 22:01:47 +0200

    Working on TomTom services

---
+++ src/iterated_variables/garagesPopups.js
@@ -0,0 +1,12 @@
+export const garagesPopups = [
+    {
+        id: 1,
+        position: [35.09, 31.84],
+        img: "1.png"
+    },
+    {
+        id: 2,
+        position: [35.23, 31.824],
+        img: "2.png"
+    },
+]
