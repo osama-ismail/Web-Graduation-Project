e90336ad61fff998f7d7df353e13fbe3e713fa80
Yazan-Habash114 <54286064+Yazan-Habash114@users.noreply.github.com>
2022-03-18 21:59:57 +0200

    Add taxi functions to map

--- src/components/Global Components/map/MainMap.jsx
+++ src/components/Global Components/map/MainMap.jsx
@@ -4,6 +4,13 @@ import { MediumScreen } from '../../responsive/Responsive';
 import { garagesPopups } from '../../../iterated_variables/garagesPopups';
 import axios from 'axios';
 
+import "./styles.css";
+import manWaving from "./img/man-waving-arm_32.png";
+import cab1 from "./img/cab1.png"
+import cab2 from "./img/cab2.png"
+import cab3 from "./img/cab3.png"
+import cab4 from "./img/cab4.png"
+
 const Container = styled.div`
     height: ${props => props.height};
     width: ${props => props.width};
@@ -16,6 +23,270 @@ let tt = null;
 let map = null;
 let markers = [];
 var locations = ""
+
+
+// From Taxi app
+const apiKey = 'q2yukmABGuRvQD9NhkGAABCOYtIMoHFD';
+
+const passengerInitCoordinates = [4.876935, 52.360306];
+
+let passengerMarker;
+
+let taxiPassengerBatchCoordinates = [];
+let taxiConfig;
+const zoomLevel = 13;
+
+let routes = [];
+let bestRouteIndex;
+
+let routeLabelsDiv;
+
+const routeWeight = 9;
+const routeBackgroundWeight = 12;
+const fastestRouteColor = '#65A7A9';
+const grayedOutDivColor = '#979797';
+
+let modal = null
+let modalContent = null
+
+function setDefaultTaxiConfig() {
+    taxiConfig = [
+        createTaxi('CAR #1', '#006967', [4.902642, 52.373627], cab1),
+        createTaxi('CAR #2', '#EC619F', [4.927198, 52.365927], cab2),
+        createTaxi('CAR #3', '#002C5E', [4.893488, 52.347878], cab3),
+        createTaxi('CAR #4', '#F9B023', [4.858433, 52.349447], cab4)
+    ];
+}
+
+function createTaxi(name, color, coordinates, iconFilePath, iconWidth = 55, iconHeight = 55) {
+    return {
+        name: name,
+        color: color,
+        icon: "<img src=" + `${iconFilePath}` + " style='width: " + iconWidth + "px; height: " + iconHeight + "px;'>",
+        coordinates: coordinates
+    };
+}
+
+function updateTaxiBatchLocations(passengerCoordinates) {
+    taxiPassengerBatchCoordinates = [];
+    taxiConfig.forEach(taxi => {
+        taxiPassengerBatchCoordinates.push(taxi.coordinates + ':' + passengerCoordinates);
+    });
+}
+
+function humanReadableTimeFormat(time) {
+    const hrs = Math.floor(time / 3600);
+    const mins = Math.floor((time % 3600) / 60);
+    const secs = time % 60;
+    let formattedString = '';
+    if (hrs > 0) {
+        formattedString += hrs + ':' + (mins < 10 ? '0' : '');
+    }
+    formattedString += mins + ':' + (secs < 10 ? '0' : '');
+    formattedString += secs;
+    return formattedString;
+}
+
+function metersToKilometers(distance) {
+    distance = distance / 1000;
+    return distance.toFixed(1);
+}
+
+function convertToPoint(lat, long) {
+    return {
+        point: {
+            latitude: lat,
+            longitude: long
+        }
+    };
+}
+
+function routeOnMouseOverAnimation(route) {
+    map.moveLayer(route[0]);
+    map.moveLayer(route[1]);
+}
+
+function bringBestRouteToFront() {
+    map.moveLayer(routes[bestRouteIndex][0]);
+    map.moveLayer(routes[bestRouteIndex][1]);
+}
+
+function buildOriginsParameter() {
+    const origins = [];
+    taxiConfig.forEach(function (taxi) {
+        origins.push(convertToPoint(taxi.coordinates[1], taxi.coordinates[0]));
+    });
+    return origins;
+}
+
+function buildDestinationsParameter() {
+    return [convertToPoint(passengerMarker.getLngLat().lat, passengerMarker.getLngLat().lng)];
+}
+
+function modifyFastestRouteColor(travelTimeInSecondsArray) {
+    const sortedTab = travelTimeInSecondsArray.slice();
+    sortedTab.sort(function (a, b) { return a - b });
+    bestRouteIndex = travelTimeInSecondsArray.indexOf(sortedTab[0]);
+    taxiConfig[bestRouteIndex].color = fastestRouteColor;
+}
+
+function displayModal() {
+    modalContent.innerText = 'Dispatch car number ' + String(bestRouteIndex + 1);
+    modal.style.display = 'block';
+}
+
+function animateRouteOnDivHover(e) {
+    routeOnMouseOverAnimation(routes[e.target.id]);
+}
+
+function updateRouteLegend(travelTimeInSecondsArray, lengthInMetersArray, trafficDelayInSecondsArray) {
+    let div;
+    routeLabelsDiv.innerText = '';
+
+    taxiConfig.forEach(function (child, index) {
+        div = document.createElement('div');
+        div.id = index;
+        div.style.borderLeftColor = child.color;
+        if (index !== bestRouteIndex) {
+            div.style.color = grayedOutDivColor;
+        }
+        div.colorHex = child.color;
+        div.innerText = child.name
+            + ' drive time:' + humanReadableTimeFormat(travelTimeInSecondsArray[index])
+            + ', distance:' + metersToKilometers(lengthInMetersArray[index])
+            + 'km, ' + ((trafficDelayInSecondsArray[index] === 0) ? 'no traffic delay' : 'traffic delay:' + humanReadableTimeFormat(trafficDelayInSecondsArray[index]));
+        div.addEventListener('mouseover', animateRouteOnDivHover);
+        div.addEventListener('mouseout', bringBestRouteToFront);
+        routeLabelsDiv.appendChild(div);
+    });
+}
+
+function buildStyle(id, data, color, width) {
+    return {
+        'id': id,
+        'type': 'line',
+        'source': {
+            'type': 'geojson',
+            'data': data
+        },
+        'paint': {
+            'line-color': color,
+            'line-width': width
+        },
+        'layout': {
+            'line-cap': 'round',
+            'line-join': 'round'
+        }
+    }
+}
+
+function drawAllRoutes() {
+    tt.services.calculateRoute({
+        batchMode: 'sync',
+        key: apiKey,
+        batchItems: [
+            { locations: taxiPassengerBatchCoordinates[0] },
+            { locations: taxiPassengerBatchCoordinates[1] },
+            { locations: taxiPassengerBatchCoordinates[2] },
+            { locations: taxiPassengerBatchCoordinates[3] }
+        ]
+    })
+        .then(function (results) {
+            results.batchItems.forEach(function (singleRoute, index) {
+                const routeGeoJson = singleRoute.toGeoJson();
+                const route = [];
+                const route_background_layer_id = 'route_background_' + index;
+                const route_layer_id = 'route_' + index;
+
+                map.addLayer(buildStyle(route_background_layer_id, routeGeoJson, 'black', routeBackgroundWeight))
+                    .addLayer(buildStyle(route_layer_id, routeGeoJson, taxiConfig[index].color, routeWeight));
+
+                route[0] = route_background_layer_id;
+                route[1] = route_layer_id;
+                routes[index] = route;
+
+                if (index === bestRouteIndex) {
+                    const bounds = new tt.LngLatBounds();
+                    routeGeoJson.features[0].geometry.coordinates.forEach(function (point) {
+                        bounds.extend(tt.LngLat.convert(point));
+                    });
+                    map.fitBounds(bounds, { padding: 150 });
+                }
+
+                map.on("mouseenter", route_layer_id, function () {
+                    map.moveLayer(route_background_layer_id);
+                    map.moveLayer(route_layer_id);
+                });
+
+                map.on("mouseleave", route_layer_id, function () {
+                    bringBestRouteToFront();
+                });
+            });
+            bringBestRouteToFront();
+        });
+}
+
+function clear() {
+    routes.forEach(function (child) {
+        map.removeLayer(child[0]);
+        map.removeLayer(child[1]);
+        map.removeSource(child[0]);
+        map.removeSource(child[1]);
+    });
+    routes = [];
+    setDefaultTaxiConfig();
+    passengerMarker.togglePopup();
+}
+
+function processMatrixResponse(result) {
+    const travelTimeInSecondsArray = [];
+    const lengthInMetersArray = [];
+    const trafficDelayInSecondsArray = [];
+    result.matrix.forEach(function (child) {
+        travelTimeInSecondsArray.push(child[0].response.routeSummary.travelTimeInSeconds);
+        lengthInMetersArray.push(child[0].response.routeSummary.lengthInMeters);
+        trafficDelayInSecondsArray.push(child[0].response.routeSummary.trafficDelayInSeconds);
+    });
+    modifyFastestRouteColor(travelTimeInSecondsArray);
+    updateRouteLegend(travelTimeInSecondsArray, lengthInMetersArray, trafficDelayInSecondsArray);
+    drawAllRoutes();
+    displayModal();
+}
+
+function callMatrix() {
+    const origins = buildOriginsParameter();
+    const destinations = buildDestinationsParameter();
+    tt.services.matrixRouting({
+        key: apiKey,
+        origins: origins,
+        destinations: destinations,
+        traffic: true
+    }).then(processMatrixResponse);
+}
+
+
+const submitButtonHandler = () => {
+    clear();
+    callMatrix();
+}
+
+function drawPassengerMarkerOnMap(geoResponse) {
+    if (geoResponse && geoResponse.addresses
+        && geoResponse.addresses[0].address.freeformAddress) {
+        passengerMarker.remove();
+        passengerMarker = createPassengerMarker(geoResponse.addresses[0].position,
+            new tt.Popup({ offset: 35 }).setHTML(geoResponse.addresses[0].address.freeformAddress));
+        passengerMarker.togglePopup();
+    }
+}
+
+function createPassengerMarker(markerCoordinates, popup) {
+    const passengerMarkerElement = document.createElement('div');
+    passengerMarkerElement.innerHTML = `<img src=${manWaving} style='width: 30px; height: 30px';>`;
+    return new tt.Marker({ element: passengerMarkerElement }).setLngLat(markerCoordinates).setPopup(popup).addTo(map);
+}
+
+// From Taxi app
 
 export var directionsRoutes = []
 
@@ -236,12 +507,44 @@ export default class App extends Component {
             // map.addTier(new tt.TrafficFlowTilesTier(trafficFlowConfig));
         });
 
+        modal = document.getElementById('modal');
+        modalContent = document.getElementById('modal-content');
+
+        routeLabelsDiv = document.getElementById('route-labels');
+
+        setDefaultTaxiConfig();
+        updateTaxiBatchLocations(passengerInitCoordinates);
+        tt.setProductInfo('Taxi dispatcher example application', '1.00');
+
+        map.addControl(new tt.NavigationControl(), 'top-left');
+        passengerMarker = createPassengerMarker(passengerInitCoordinates,
+            new tt.Popup({ offset: 35 }).setHTML("Click anywhere on the map to change passenger location."));
+        passengerMarker.togglePopup();
+        taxiConfig.forEach(function (taxi) {
+            const carMarkerElement = document.createElement('div');
+            carMarkerElement.innerHTML = taxi.icon;
+            new tt.Marker({ element: carMarkerElement, offset: [0, 27] }).setLngLat(taxi.coordinates).addTo(map);
+        });
+
         map.on('click', (event) => {
-            this.addMarkerOnClick(event, {
-                color: 'rgb(190, 18, 47)',
-                width: '40',
-                height: '50'
-            });
+            // this.addMarkerOnClick(event, {
+            //     color: 'rgb(190, 18, 47)',
+            //     width: '40',
+            //     height: '50'
+            // });
+            const position = event.lngLat;
+            tt.services.reverseGeocode({
+                key: apiKey,
+                position: position
+            })
+                .then(function (results) {
+                    drawPassengerMarkerOnMap(results);
+                    updateTaxiBatchLocations(passengerMarker.getLngLat().toArray());
+                });
+        });
+
+        modal.addEventListener('click', function () {
+            modal.style.display = 'none';
         });
     }
 
@@ -252,7 +555,16 @@ export default class App extends Component {
                 width={this.props.width}
                 height={this.props.height}
                 borderRadius={this.props.borderRadius}
-            />
+            >
+                <div id="labels-container">
+                    <label>Find the taxi that will arrive fastest</label>
+                    <div id="route-labels"></div>
+                    <input type="button" id="submit-button" value="Submit" onClick={submitButtonHandler} />
+                </div>
+                <div id="modal">
+                    <div id="modal-content"></div>
+                </div>
+            </Container>
         );
     }
 }
